eebdfc7825248a4579781f5f1a17baa38702363a
Ajani Bilby <aj@bilby.net>
2018-07-07 13:23:23 +1000

    + Build cmd parameter intake

--- build.bat
+++ build.bat
@@ -2,25 +2,49 @@ echo off
 rem -std=c++11
 
 
-:question
-set INPUT=
-set /P INPUT=Build with memory safety? (y/n) %=%
-
-if /I "%INPUT%"=="y" goto safe
-if /I "%INPUT%"=="n" goto unsafe
-
-echo Invalid input "%INPUT%"
-goto question
 
 
+rem Use first arg as first question response
+set MemSafe=%1
+shift
 
-:unsafe
-clang++ "./code/main.cpp" -o "./fiber.exe"
+rem Construct the command as the build command is configured
+set Command=clang++ "./code/main.cpp" -o "./fiber.exe"
+
+
+
+
+if /I "%MemSafe%"=="y" goto SetMemSafe
+if /I "%MemSafe%"=="n" goto end
+:QuestionMemSafe
+set MemSafe=
+set /P MemSafe=Build with memory safety? (y/n) %=%
+
+if /I "%MemSafe%"=="y" goto SetMemSafe
+if /I "%MemSafe%"=="n" goto end
+
+echo Invalid input "%MemSafe%"
+goto QuestionMemSafe
+
+
+:SetMemSafe
+set Command=%Command% -D MemorySafe
 goto end
 
 
-:safe
-clang++ -D MemorySafe "./code/main.cpp" -o "./fiber.exe"
-goto end
+
+
 
 :end
+echo Settings;
+IF /I "%MemSafe%"=="y" (
+  echo  - Memory Safety : Yes
+) ELSE (
+  echo  - Memory Safety : No
+)
+echo  - 64bit         : Yes
+echo  - OS            : Native
+
+echo.
+echo %Command%
+%Command%
