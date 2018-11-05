e7f1c165593095ab872528269f45dabdb557c416
Ajani Bilby <aj@bilby.net>
2018-11-05 12:35:17 +1100

    +Interp child func consuming

--- source/lib/interprete/function.cpp
+++ source/lib/interprete/function.cpp
@@ -1,20 +1,97 @@
 #include "./function.hpp"
 
-Function::Function(std::string name, std::vector<RawAction> tokens){
+Function::Function(std::string name, std::vector<RawAction> tokens, unsigned long domainSize, Function* owner){
+	this->domain = domainSize;
+	this->parent = owner;
 	this->valid = true;
 	this->name = name;
-	
-	Interpreter::Action temp;
-	
+
+
+
+	// Interpret raw tokens
 	unsigned long size = tokens.size();
 	for (unsigned long i=0; i<size; i++){
-		temp = Interpreter::Convert(tokens[i]);
-		
+		// Forward any child functions through to a new child
+		if (tokens[i].param[0] == "func"){
+			std::vector<RawAction> forward;
+			unsigned long depth = 0;
+
+			if (tokens[i].param.size() != 3){
+				std::cerr << "Error: Invalid number of arguments during function definition" << std::endl;
+				std::cerr << "  line: " << tokens[i].line << std::endl;
+				this->valid = false;
+				break;
+			}
+
+			auto lsize = Interpreter::Opperand(tokens[i].param[2]);
+			if ( lsize.valid == false || (lsize.type != Interpreter::OpperandType::Bytes && lsize.type != Interpreter::OpperandType::Uint) ){
+				std::cerr << "Error: Invalid function domain size. Domain size must be specified via a hex or uint value" << std::endl;
+				std::cerr << "    arg: " << tokens[i].param[2] << std::endl;
+				std::cerr << "   type: " << lsize.typeToString() << std::endl;
+				std::cerr << "  valid: " << lsize.valid << std::endl;
+				std::cerr << "   line: " << tokens[i].line << std::endl;
+				this->valid = false;
+				break;
+			}
+
+			if (i+1 >= size || tokens[i+1].param[0] != "{"){
+				std::cerr << "Error: Invalid function definition, missing opening bracket on new line" << std::endl;
+				std::cerr << "  line: " << tokens[i].line << std::endl;
+				this->valid = false;
+				break;
+			}
+
+			// Find the closing bracket at the same depth
+			forward.resize(0);
+			depth = 1;
+			unsigned long j=i+2;
+			for (; j<size; j++){
+				if (tokens[j].param[0] == "{"){
+					depth++;
+				}else if (tokens[j].param[0] == "}"){
+					depth--;
+				}
+
+				if (depth == 0){
+					break;
+				}
+
+				forward.push_back(tokens[j]);
+			}
+
+			if (tokens[j].param[0] != "}"){
+				std::cerr << "Error: Invalid function definition, missing closing bracket" << std::endl;
+				std::cerr << "  line: " << tokens[i].line << std::endl;
+				this->valid = false;
+				break;
+			}
+
+			this->child.push_back( Function(tokens[i].param[1], forward, lsize.data.uint64, this) );
+			i = j;
+			continue;
+		}
+
+		Action temp;
+		temp = Interpreter::Convert(tokens[i], this);
+
 		if (temp.cmd == Command::invalid){
 			this->valid = false;
+			return;
 		}
-		
+
 		this->code.append(temp);
 	}
 	this->code.simplifyJumps();
+
+
+
+	// Check if any child is invalid
+	// If so make this function invalid
+	size = this->child.size();
+	for (unsigned long i=0; i<size; i++){
+		if (this->child[i].valid == false){
+			this->valid = false;
+			return;
+		}
+	}
 };
--- source/lib/interprete/function.hpp
+++ source/lib/interprete/function.hpp
@@ -1,23 +1,24 @@
 #pragma once
 
-#include "./../interprete/tokenize.hpp"
 #include "./../execution/bytecode.hpp"
 
 #include <string>
 #include <vector>
 
-
 class Function{
 	public:
+		Function* parent;
 		std::string name;
-	
+
 		Bytecode code;
 		std::vector<Function> child;
-		
+
 		bool valid;
-		
-		Function(std::string name, std::vector<RawAction> tokens);
+		unsigned long domain;
+
+		Function(std::string name, std::vector<RawAction> tokens, unsigned long domainSize, Function* owner=nullptr);
 };
 
 
+#include "./../interprete/interpreter.hpp"
 #include "./function.cpp"
